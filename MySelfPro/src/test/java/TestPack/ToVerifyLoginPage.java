ffc9e39b0f5a3095a9e340b0253b03fec73f0159
randhepankaj <130379726+randhepankaj@users.noreply.github.com>
2024-07-30 08:50:40 +0530

    Update ToVerifyLoginPage.java
    added pankaj randhe

--- MySelfPro/src/test/java/TestPack/ToVerifyLoginPage.java
+++ MySelfPro/src/test/java/TestPack/ToVerifyLoginPage.java
@@ -61,7 +61,8 @@ public class ToVerifyLoginPage extends Base {
 		loginpage.enterUsename();
 		loginpage.enterPassword();
 		loginpage.clickOnLoginButton();
-		
+				System.out.println("pankaj randhe");
+
 		
 	}
 	
